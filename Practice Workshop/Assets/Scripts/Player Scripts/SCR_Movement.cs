411029adb9b4d5372e923c64d4ad55c9b14693b3
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2021-02-23 12:02:14 -0800

    Updated plus got is grounded working
    added more lines of code to movement script

--- Practice Workshop/Assets/Scripts/Player Scripts/SCR_Movement.cs
+++ Practice Workshop/Assets/Scripts/Player Scripts/SCR_Movement.cs
@@ -6,14 +6,15 @@ public class SCR_Movement : MonoBehaviour
 {
 
     public float speed = 5;
-    private Rigidbody rb;
-    private BoxCollider BC;
+    private Rigidbody2D rb;
+    private BoxCollider2D BC;
     [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] private GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        BC = transform.GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody2D>();
+        BC = transform.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,11 +28,20 @@ public class SCR_Movement : MonoBehaviour
             rb.velocity = Vector2.up * jumpVel;
 
         }
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            shoot();
+        }
     }
 
     private bool IsGrounded()
     {   
         RaycastHit2D ray = Physics2D.BoxCast(BC.bounds.center, BC.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
         return ray.collider != null;
+    }
+
+    private void shoot()
+    {
+        Instantiate(bullet, transform.position, Quaternion.identity);
     }
 }
