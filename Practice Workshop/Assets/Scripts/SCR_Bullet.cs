c92e70d16ecc167a3e0691c612414f6f4e7b7946
emmerdroid <58455108+emmerdroid@users.noreply.github.com>
2021-02-24 15:02:04 -0800

    updated bullet

--- Practice Workshop/Assets/Scripts/SCR_Bullet.cs
+++ Practice Workshop/Assets/Scripts/SCR_Bullet.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class SCR_Bullet : MonoBehaviour
 {
+    public float speed = 15f;
+    public Rigidbody2D bullet;
+    public int damage = 2;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        bullet.velocity = transform.right * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other) 
     {
-        
+        Destroy(gameObject);
     }
 }
